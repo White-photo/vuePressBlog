3d954eefdc7621befca05a37dee7455b10b288d2
huahua <2093839790@qq.com>
2021-12-09 17:42:58 +0800

    仓库1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,31 @@
 module.exports = {
     theme: 'reco',
+    themeConfig: {
+        // 博客配置
+        blogConfig: {
+            category: {
+                location: 2,     // 在导航栏菜单中所占的位置，默认2
+                text: 'Category' // 默认文案 “分类”
+            },
+            nav: [
+                { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' }
+            ],
+            tag: {
+                location: 3,     // 在导航栏菜单中所占的位置，默认3
+                text: 'Tag'      // 默认文案 “标签”
+            },
+            socialLinks: [     // 信息栏展示社交信息
+                { icon: 'reco-github', link: 'https://github.com/recoluan' },
+                { icon: 'reco-npm', link: 'https://www.npmjs.com/~reco_luan' }
+            ],
+            // 密钥
+            keyPage: {
+                keys: ['32位的 md5 加密密文'], // 1.3.0 版本后需要设置为密文
+                color: '#42b983', // 登录页动画球的颜色
+                lineColor: '#42b983' // 登录页动画线的颜色
+            }
+        }
+    },
     title: 'Chen\'s blog',
     description: '我的个人网站',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
--- docs/test1.md
+++ docs/test1.md
@@ -1,7 +1,21 @@
-1. First item
-2. Second item
-3. Third item
-4. Fourth item
+---
+title: 烤鸭的做法
+date: '2019-08-08 08:00:00'
+sidebar: 'auto'
+categories:
+- 烹饪
+- 爱好
+  tags:
+- 烤
+- 鸭子
+  keys:
+- '123456'
+  publish: false
+---
+2. First item
+3. Second item
+4. Third item
+5. Fourth item
 > list null
 > >golang.ps
 * st
@@ -17,9 +31,9 @@
 > 
 # tip
 *sda* sda **sda**
->   function() {
->       let arr = [];
->       if (arr.length === 1) {
->           goland();
->        }
->    }
+    function() {
+        let arr = [];
+        if (arr.length === 1) {
+            goland(); 
+        }
+    }
