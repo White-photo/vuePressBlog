cae54c60ef6eaf25610cbec76547784e5a1bb643
huahua <2093839790@qq.com>
2021-12-22 16:51:51 +0800

    仓库1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,14 +37,5 @@ module.exports = {
     markdown: {
         lineNumbers: false // 代码块显示行号
     },
-    themeConfig: {
-        nav:[ // 导航栏配置
-            {text: '前端基础', link: '/accumulate/' },
-            {text: '算法题库', link: '/algorithm/'},
-            {text: '微博', link: 'https://baidu.com'}
-        ],
-        sidebar: 'auto', // 侧边栏配置
-        sidebarDepth: 2, // 侧边栏显示2级
-    },
     serviceWorker: true // 是否开启 PWA
 };
