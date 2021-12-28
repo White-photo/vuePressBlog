f0c1876f0d321c8300f0474b754248f939dc36d5
huahua <2093839790@qq.com>
2021-12-28 11:43:29 +0800

    安装reco插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
     title: "vuepress-theme-reco",  //这里是博客标题
     description: 'A simple and beautiful vuepress blog theme .',  //博客描述
@@ -46,6 +47,87 @@ module.exports = {
                 text: 'Tag' // 默认 “标签”
             }
         },
+        plugins: [
+            [
+                //先安装在配置， npm install @vuepress-reco/vuepress-plugin-kan-ban-niang --save
+                "@vuepress-reco/vuepress-plugin-kan-ban-niang",
+                {
+                    theme: ['blackCat', 'whiteCat', 'haru1', 'haru2', 'haruto', 'koharu', 'izumi', 'shizuku', 'wanko', 'miku', 'z16'],
+                    clean: false,
+                    messages: {
+                        welcome: '我是lookroot欢迎你的关注 ',
+                        home: '心里的花，我想要带你回家。',
+                        theme: '好吧，希望你能喜欢我的其他小伙伴。',
+                        close: '再见哦'
+                    },
+                    width: 240,
+                    height: 352
+                }
+            ],
+            [
+                //鼠标点击特效 先安装在配置， npm install vuepress-plugin-cursor-effects --save
+                "cursor-effects",
+                {
+                    size: 3,                    // size of the particle, default: 2
+                    shape: ['circle'],  // shape of the particle, default: 'star'
+                    zIndex: 999999999           // z-index property of the canvas, default: 999999999
+                }
+            ],
+            [
+                //动态标题 先安装在配置， npm install vuepress-plugin-dynamic-title --save
+                "dynamic-title",
+                {
+                    showIcon: "/favicon.ico",
+                    showText: "(/≧▽≦/)咦！又好了！",
+                    hideIcon: "/failure.ico",
+                    hideText: "(●—●)喔哟，崩溃啦！",
+                    recoverTime: 2000
+                }
+            ],
+            [
+                //图片放大插件 先安装在配置， npm install @vuepress\plugin-medium-zoom --save
+                '@vuepress\plugin-medium-zoom', {
+                selector: '.page img',
+                delay: 1000,
+                options: {
+                    margin: 24,
+                    background: 'rgba(25,18,25,0.9)',
+                    scrollOffset: 40
+                }
+            }
+            ],
+            [
+                //插件广场的流程图插件 先安装在配置 npm install vuepress-plugin-flowchart --save
+                'flowchart'
+            ],
+            [
+                //插件广场的sitemap插件 先安装在配置 npm install vuepress-plugin-sitemap --save
+                'sitemap', {
+                hostname: 'https://www.glassysky.site'
+            }
+            ],
+            ["vuepress-plugin-nuggets-style-copy", {
+                copyText: "复制代码",  //vuepress复制粘贴提示插件P 先安装在配置 npm install vuepress-plugin-nuggets-style-copy --save
+                tip: {
+                    content: "复制成功!"
+                }
+            }],
+            ["@vuepress-yard/vuepress-plugin-window",{
+                title: "远方有你伴余生の公告",  //vuepress公告插件 先安装在配置 npm install @vuepress-yard/vuepress-plugin-window --save
+                contentInfo: {
+                    title: "欢迎进来的小耳朵 🎉🎉🎉",
+                    needImg: true,
+                    imgUrl: "https://reinness.com/avatar.png",
+                    content: "喜欢博皮可以到博客园关注教程",
+                    contentStyle: ""
+                },
+                bottomInfo: {
+                    btnText: '关于',
+                    linkTo: 'https://cnblogs.com/glassysky'
+                },
+                closeOnce: false
+            }]
+        ],
         //友情链接
         friendLink: [
             {
